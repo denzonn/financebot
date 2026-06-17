55ccb2082be92eaa879b3ce0a836d08ee5eac99c
denzonn <denson753@gmail.com>
2026-06-18 02:03:34 +0800

    Update

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|
+|s
 */
 
 Route::post('/telegram/webhook', TelegramController::class);
